66704483ac94f1afd69f0fac5d3946d8f759ff35
Rafalo1212 <a.rivendare@o2.pl>
2018-03-04 19:23:38 +0100

    new

--- lab0/lab0.py
+++ lab0/lab0.py
@@ -16,8 +16,8 @@ for i,line in enumerate(file):
 		n = int(line.split()[0])
 		m = int(line.split()[1])
 		array = [[0 for x in range(m)] for y in range(n)]
-		cmax_tab = [0 for x in range(m)]
-		cmax_after=[[0 for x in range(m)] for y in range(n)]
+                cmax_tab=[0 for x in range(m)]
+                cmax_after=[[0 for x in range(m)] for y in range(n)]
 	else:
 		for v in range(m):
 			array[i-1][v] = int(line.split()[v])
@@ -28,19 +28,20 @@ q=0
 
 for v in range(m):
 	for i in range(n):
-		if v==0:
-			cmax_tab[v]+=array[i][v]
-			cmax_after[i][0]=cmax_tab[v]
-		elif i==0 and not v==0:
-			q=v
-		while not q==-1:
-			cmax_tab[v]+=array[0][q]
-			cmax_after[i][v]=cmax_tab[v]
-			q-=1
-		else:          
-			tmp1=max(cmax_tab[v],cmax_after[i][v-1])
-			cmax_tab[v]+=(tmp1-cmax_tab[v])+array[i][v]
-			cmax_after[i][v]=cmax_tab[v]
+                if v==0:
+                        cmax_tab[v]+=array[i][v]
+                        cmax_after[i][0]=cmax_tab[v]
+                elif i==0 and not v==0:
+                        q=v
+                        while not q==-1:
+                                cmax_tab[v]+=array[0][q]
+                                cmax_after[i][v]=cmax_tab[v]
+                                q-=1
+                else:
+                        
+                        tmp1=max(cmax_tab[v],cmax_after[i][v-1])
+                        cmax_tab[v]+=(tmp1-cmax_tab[v])+array[i][v]
+                        cmax_after[i][v]=cmax_tab[v]
                         
 print(cmax_tab)
-print ('Najkrotszy czas wykonania to %s' %(max(cmax_tab)))
+print 'Najkrotszy czas wykonania to',max(cmax_tab)
