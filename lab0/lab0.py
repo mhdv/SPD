08a75db3172ad31996ef3cf850b2acd064800c8b
Rafalo1212 <a.rivendare@o2.pl>
2018-03-04 21:20:02 +0100

    algortm permutacji

--- lab0/lab0.py
+++ lab0/lab0.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import itertools
 
 n = 0
 m = 0
@@ -26,22 +27,35 @@ time = 0
 c = 0
 q=0
 
-for v in range(m):
-	for i in range(n):
-                if v==0:
-                        cmax_tab[v]+=array[i][v]
-                        cmax_after[i][0]=cmax_tab[v]
-                elif i==0 and not v==0:
-                        q=v
-                        while not q==-1:
-                                cmax_tab[v]+=array[0][q]
-                                cmax_after[i][v]=cmax_tab[v]
-                                q-=1
-                else:
+perms=list(itertools.permutations(array))
+cmax_total=0;
+
+array=perms[0]
+
+for p in range(len(perms)):
+        array=perms[p]
+        cmax_tab=[0 for x in range(m)]
+        for v in range(m):
+	        for i in range(n):
+                        if v==0:
+                                cmax_tab[v]+=array[i][v]
+                                cmax_after[i][0]=cmax_tab[v]
+                        elif i==0 and not v==0:
+                                q=v
+                                while not q==-1:
+                                        cmax_tab[v]+=array[0][q]
+                                        cmax_after[i][v]=cmax_tab[v]
+                                        q-=1
+                        else:
                         
-                        tmp1=max(cmax_tab[v],cmax_after[i][v-1])
-                        cmax_tab[v]+=(tmp1-cmax_tab[v])+array[i][v]
-                        cmax_after[i][v]=cmax_tab[v]
-                        
-print(cmax_tab)
-print 'Najkrotszy czas wykonania to',max(cmax_tab)
+                                        tmp1=max(cmax_tab[v],cmax_after[i][v-1])
+                                        cmax_tab[v]+=(tmp1-cmax_tab[v])+array[i][v]
+                                        cmax_after[i][v]=cmax_tab[v]
+        if p==0:
+                cmax_total=max(cmax_tab)
+        else:
+                cmax_total=min(cmax_total,max(cmax_tab))
+        if max(cmax_tab)==637:
+                print cmax_tab
+                print array
+print 'Najkrotszy czas wykonania to',cmax_total
